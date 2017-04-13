cdadbc1e68bd71663ae3935845d5faddb35aff38
junhan605 <junahn605@mail.com>
2017-04-13 11:39:06 +0800

    upload

---
+++ lib/shopify_app/version.rb
@@ -0,0 +1,3 @@
+module ShopifyApp
+  VERSION = '7.2.6'
+end
---
+++ test/dummy/config/initializers/shopify_app.rb
@@ -0,0 +1,7 @@
+ShopifyApp.configure do |config|
+  config.api_key = "key"
+  config.secret = "secret"
+  config.scope = 'read_orders, read_products'
+  config.embedded_app = true
+  config.myshopify_domain = 'myshopify.com'
+end
